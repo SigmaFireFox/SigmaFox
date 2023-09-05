971e83cfc5428fb980709df36e090d7206b41b87
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2023-09-05 17:34:30 +0200

    Removing Ferric-Tech

--- apps/sigmafox/src/app/app.component.ts
+++ apps/sigmafox/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'sigmafox-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sigmafox';
+  title = 'Ferric Tech';
+
+  constructor(private router: Router) {}
+
+  onLogoclick() {
+    this.router.navigate(['']);
+  }
 }
