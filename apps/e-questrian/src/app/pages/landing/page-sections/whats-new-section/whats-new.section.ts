8769f1ff4507a7cd9deea4ea942e3bb17a8169f6
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-01-19 10:17:55 +0200

    e-Questrian/pages/landing: Whats new section content added

--- apps/e-questrian/src/app/pages/landing/page-sections/whats-new-section/whats-new.section.ts
+++ apps/e-questrian/src/app/pages/landing/page-sections/whats-new-section/whats-new.section.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonSize } from '@sigmafox/buttons';
 
 export const BackgroundColours = [
   'lime',
@@ -11,7 +13,7 @@ export const BackgroundColours = [
 
 export interface WhatsNewScreenContent {
   image: string;
-  content: string;
+  content: string[];
 }
 
 @Component({
@@ -26,16 +28,29 @@ export class WhatsNewSection {
   screens: WhatsNewScreenContent[] = [
     {
       image: 'horse-head-icon.png',
-      content: 'ksjdhfkshdkfjhs',
+      content: [
+        'At e-Questrian, we acknowledge that no app can ever \
+      be perfect and that users will always a little something extra \
+      here and a little something extra there, and we are okay with \
+      that. We have the view that the perfect app is not a distination \
+      - its a journey, and one we love taking.',
+        'So in light of our philosophy we endevour to constantly improve \
+      our app to meet the needs our users with out making the app more \
+      difficult to use. So we are constantly making changes and improvements.',
+        'If you are curious to what are the latest changes we have made - \
+      please feel free to view our change logs ',
+      ],
     },
   ];
 
+  buttonSize = ButtonSize;
   screenCounter = 0;
   currentHeader = 'Whats new? \n (Product updates)';
-  currentCopy = '';
+  currentCopy: string[] = [];
   currentImage = '';
+  callToActionText = 'View change logs';
 
-  constructor() {
+  constructor(private router: Router) {
     this.switchScreen();
     setInterval(() => {
       this.switchScreen();
@@ -44,6 +59,10 @@ export class WhatsNewSection {
 
   scrollToNext() {
     this.scrollToNextScreen.emit();
+  }
+
+  callToActionClick() {
+    throw new Error('Method not implemented.');
   }
 
   private switchScreen() {
