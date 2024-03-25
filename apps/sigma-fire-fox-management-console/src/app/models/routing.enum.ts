11bd3d3dc299206ad22d71e5d3fa99efe6818f07
Ruben Ferreira <ruben.ferreira@momentum.co.za>
2024-03-25 13:49:20 +0200

    SFFMC - Project updated to run with routing

---
+++ apps/sigma-fire-fox-management-console/src/app/models/routing.enum.ts
@@ -0,0 +1,9 @@
+export enum AppRoutePaths {
+  LandingPage = '',
+  SignIn = 'signin',
+  Register = 'register',
+  Dashboard = 'dashboard',
+  Calendar = 'calendar',
+  Finances = 'finances',
+  Clients = 'clients',
+}
---
+++ apps/sigma-fire-fox-management-console/src/app/pages/landing/landing-page.component.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @angular-eslint/component-class-suffix */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sigmafox-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss'],
+})
+export class LandingPage {}
